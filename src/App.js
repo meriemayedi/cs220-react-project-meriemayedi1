da7f024130ea7df31ef26ee08379e09eaea56ee5
meriem <maryemayadi244@gmail.com>
2025-05-18 17:14:35 +0100

    W4: Multiple components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import * as React from "react";
-
 const list = [
-  // same list as Week 3
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
 ];
 
 function Search() {
