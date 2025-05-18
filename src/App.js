f832ee7a913507a9b7c29c7369802a60ac5218c3
meriem <maryemayadi244@gmail.com>
2025-05-18 16:15:39 +0100

    W2: JSX and variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import * as React from "react";
 
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+
+function getTitle(title) {
+  return title;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>
+        {welcome.greeting} {getTitle("React")}
+      </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
